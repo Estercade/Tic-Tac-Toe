47782cf706689ad0fd3e919fedbb426764748c35
Estercade <withrwithoutyou@gmail.com>
2023-06-28 19:08:18 -0400

    Add logic to determine winner or a tie

--- js/index.js
+++ js/index.js
@@ -4,6 +4,36 @@ const playerFactory = function(name, marker) {
     return { name, marker }
 }
 
+const player1 = playerFactory("player1", "X");
+const player2 = playerFactory("player2", "O");
+
+const turnHandler = (function() {
+    let currentPlayer = player1;
+    let turnCounter = 1;
+
+    function changeTurn() {
+        currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+        turnCounter++;
+    }
+
+    function resetTurns(e) {
+        if (e.type="restartGame") {
+            currentPlayer = player1;
+            turnCounter = 1;
+        }
+    }
+
+    function currentTurn() {
+        return currentPlayer;
+    }
+
+    function turnCount() {
+        return turnCounter;
+    }
+
+    return { changeTurn, resetTurns, currentTurn, turnCount };
+})();
+
 const gameboardHandler = (function() {
     let gamestate = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
 
@@ -15,8 +45,9 @@ const gameboardHandler = (function() {
             let clone = template.content.cloneNode(true);
             let li = clone.querySelector("li");
             li.innerText = gamestate[i];
-            _attachListener(li);
+            li.id = `gameboardSquare${i}`;
             gameboard.append(clone);
+            _attachListener(li);
         }
     }
 
@@ -24,15 +55,22 @@ const gameboardHandler = (function() {
         gameboardSquare.addEventListener('click', _placeMarker);
     }
 
-    const event = new Event("markerPlaced");
+    const markerPlacedEvent = new Event("markerPlaced");
 
     function _placeMarker(e) {
         if (e.target.innerText !== "") {
             console.log("That square is not empty!");
         } else {
-            dispatchEvent(event);
+            let currentPlayerMarker = turnHandler.currentTurn().marker;
+            let gameboardLocation = e.target.id.substring(15);
+            gamestate[gameboardLocation] = currentPlayerMarker;
+            e.target.innerText = currentPlayerMarker;
+            _checkGamestate();
+            dispatchEvent(markerPlacedEvent);
         }
     }
+
+    const restartGameEvent = new Event("restartGame");
 
     function clearGameboard() {
         _clearGamestate();
@@ -41,6 +79,7 @@ const gameboardHandler = (function() {
             gameboard.removeChild(gameboard.firstChild);
         }
         render();
+        dispatchEvent(restartGameEvent);
     }
 
     const restartButton = document.getElementById("restart-button");
@@ -48,22 +87,54 @@ const gameboardHandler = (function() {
 
     function _clearGamestate() {
         gamestate.fill("", 0);
-        console.log(gamestate);
+    }
+
+    function _checkGamestate() {
+        let winner;
+        
+        // Check for diagonal win
+        if (gamestate[0] !== "" && gamestate[0] == gamestate[4] && gamestate[0] == gamestate[8]) {
+            decideWinner(gamestate[0]);
+        } else if (gamestate[2] !== "" && gamestate[2] == gamestate[4] && gamestate[0] == gamestate[6]) {
+            decideWinner(gamestate[0]);
+        }
+
+        // Check for horizontal and vertical win
+        for (let i = 0; i < 7; i++) {
+            if (gamestate[i] == "") {
+                continue;
+            } else if (gamestate[i] == gamestate[i+1] && gamestate[i] == gamestate[i+2]) {
+                decideWinner(gamestate[i]);
+                break;
+            } else if (gamestate[i] == gamestate[i+3] && gamestate[i] == gamestate[i+6]) {
+                decideWinner(gamestate[i]);
+                break;
+            }
+        }
+
+        // If board is filled, declare a tie
+        if (turnHandler.turnCount() == 9) {
+            console.log("Tie!");
+        }
+    }
+
+    function decideWinner(marker) {
+        if (marker == player1.marker) {
+            console.log(`${player1.name} wins!`)
+        } else {
+            console.log(`${player2.name} wins!`)
+        }
     }
 
     return { render, clearGameboard };
 })();
 
-const turnHandler = (function() {
-    function marker() {
-        console.log("Marker has been placed.");
-    }
+(function() {
+    window.addEventListener("markerPlaced", turnHandler.changeTurn);
 
-    return { marker };
+    window.addEventListener("markerPlaced", turnHandler.changeTurn);
+    
+    window.addEventListener("restartGame", turnHandler.resetTurns);
 })();
 
 gameboardHandler.render();
-
-(function() {
-    window.addEventListener("markerPlaced", turnHandler.marker);
-})();
